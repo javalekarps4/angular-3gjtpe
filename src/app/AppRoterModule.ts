f99168128f3821b9972ec96cc8e872e30bc436dc
javalekarps4 <javalekarps4@gmail.com>
2019-04-09 11:13:03 +0530

    1 file added

---
+++ src/app/AppRoterModule.ts
@@ -0,0 +1,8 @@
+import {LoginComponent}from './login/login.component';
+import{RegisterComponent}from './register/register.component';
+import{RouterModule,Routes}from'@angular/router';
+import{NgModule}from'@angular/core';
+
+const AppRoutes:Routes=[
+  {}
+];
