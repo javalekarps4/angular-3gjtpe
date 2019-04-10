f6f38b524fe3995b303c0b47380a3174a6eb7f96
javalekarps4 <javalekarps4@gmail.com>
2019-04-10 17:49:34 +0530

    2 files modified

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import{FormBuilder,Validators}from'@angular/forms';
 
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
--- src/app/shared/customvalidators.ts
+++ src/app/shared/customvalidators.ts
@@ -1,4 +1,8 @@
 
- export function ForbiddenUsernames(){
-
+import{AbstractControl,ValidatorFn}from'@angular/forms';
+ export function forbiddenNameValidator(name: RegExp): ValidatorFn {
+  return (control: AbstractControl): {[key: string]: any} | null => {
+    const forbidden = name.test(control.value);
+    return forbidden ? {'forbiddenName': {value: control.value}} : null;
+  };
 }
