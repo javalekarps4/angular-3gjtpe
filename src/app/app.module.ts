8545653e1af37c328a81e399b5a9863b9e8aef22
javalekarps4 <javalekarps4@gmail.com>
2019-04-15 11:43:30 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,15 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import{ReactiveFormsModule}from'@angular/forms'
 import{AppRouterModule}from './AppRoterModule';
 import { MenuComponent } from './menu/menu.component';
+import { AuthServiceService } from './auth-service.service';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,AppRouterModule,ReactiveFormsModule ],
   declarations: [ AppComponent, HelloComponent, LoginComponent, RegisterComponent, PagenotfoundComponent, WelcomeComponent, MenuComponent ],
  
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+ 
+  providers: [AuthServiceService]
 })
 export class AppModule { }
