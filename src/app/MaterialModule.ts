1d629c05bebbea3a49f1545089a51ef619641c9e
javalekarps4 <javalekarps4@gmail.com>
2019-07-02 11:56:15 +0530

    2 files modified

---
+++ src/app/MaterialModule.ts
@@ -0,0 +1,8 @@
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+@NgModule({
+  declarations:[],
+  imports:[],
+  providers:[],
+  bootstrap:[]
+})
+export class MaterialModule{}
